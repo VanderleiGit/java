09d461d244b7649be40edb01fb1dcc5090c2525f
vanderlei júnior <104507938+Vanderlei23@users.noreply.github.com>
2022-06-14 21:10:59 -0300

    Add files via upload

---
+++ package br.com.geekuniverty.secao03;.java
@@ -0,0 +1,25 @@
+package br.com.geekuniverty.secao03;
+
+import java.util.Scanner;
+
+public class Exercicio07 {
+
+    public static void main(String[] args) {
+    //variaáveis
+    float altura, peso ideal;
+    Scanner teclado = new Scanner(System.in);
+
+
+    //Entradas
+    System.out.println("Qual a sua altra?");
+    altura = teclado.nextfloat();
+
+    //Processamento 
+    peso_ideal = (float)(altura * 72.7) - 58; //cast
+
+    //saida
+    System.out.println("Seu peso ideal seria" + peso_ideal)
+
+    teclado.close();
+}
+}
